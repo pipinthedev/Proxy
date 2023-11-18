5f54f72a281efd3c57630af9a264bfeeb4014f12
Pranesh [ PIPIN ] <117482836+pipinthedev@users.noreply.github.com>
2023-11-19 05:18:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 install_3proxy() {
-	echo "installing 3proxy"
-	URL="https://github.com/z3APA3A/3proxy/archive/0.8.13.tar.gz"
-	wget -qO- $URL | tar -xzvf -
-	cd 3proxy-0.8.13
-	make -f Makefile.Linux
-	mkdir -p /usr/local/etc/3proxy/bin
-	mkdir -p /usr/local/etc/3proxy/logs
-	mkdir -p /usr/local/etc/3proxy/stat
-	cp bin/3proxy /usr/local/etc/3proxy/bin/
-	cp scripts/rc.d/proxy.sh /etc/init.d/3proxy
-	chmod +x /etc/init.d/3proxy
-	systemctl enable 3proxy
-	cd $WORKDIR
+    echo "installing 3proxy"
+    URL="https://github.com/z3APA3A/3proxy/archive/0.8.13.tar.gz"
+    wget -qO- $URL | tar -xzvf -
+    cd 3proxy-0.8.13
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/bin
+    mkdir -p /usr/local/etc/3proxy/logs
+    mkdir -p /usr/local/etc/3proxy/stat
+    cp src/3proxy /usr/local/etc/3proxy/bin/   # <-- Change this line
+    cp scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    systemctl enable 3proxy
+    cd $WORKDIR
 }
 
+
 gen_3proxy() {
-	cat <<EOF
+    cat <<EOF
 daemon
 maxconn 1000
 nscache 65536
@@ -51,35 +52,36 @@ EOF
 }
 
 gen_proxy_file_for_user() {
-	cat >proxy.txt <<EOF
+    cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
 upload_proxy() {
-	local PASS=$(random)
-	zip --password $PASS proxy.zip proxy.txt
-	URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+    local PASS=$(random)
+    command -v zip || apt-get install -y zip
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
 
-	echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-	echo "Download zip archive from: ${URL}"
-	echo "Password: ${PASS}"
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
 }
 
 gen_data() {
-	seq $FIRST_PORT $LAST_PORT | while read port; do
-		echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
-	done
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
 }
 
 gen_iptables() {
-	cat <<EOF
+    cat <<EOF
 $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
 gen_ifconfig() {
-	cat <<EOF
+    cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
@@ -100,8 +102,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
+read -p "How many proxy do you want to create? Example 500: " COUNT
 
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
@@ -117,7 +118,7 @@ cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
-service 3proxy start
+systemctl start 3proxy
 EOF
 
 bash /etc/rc.local
